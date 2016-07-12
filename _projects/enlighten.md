22270a787080d5900c747d69d83cbe9bc55c834f
LenKagamine <micklepickle98@gmail.com>
2016-07-12 04:09:37 -0400

    Add About and Project content

---
+++ _projects/enlighten.md
@@ -0,0 +1,14 @@
+---
+layout: project
+title: Enlighten
+description: Collaborative multiplayer webgame
+tools: javascript, nodejs, socketio
+github: Enlighten
+link: enlighten-game.herokuapp.com
+---
+
+Enlighten is a multiplayer webgame, using Node.js and Socket.io for multiple players, and hosted on Heroku's PAAS.
+
+Inspired by other webgames, Enlighten is centered around cooperation; rather than players competing against each other, they band together to advance. Hence, the more players online, the easier it is to traverse the levels.
+
+Initially, I started this project to learn JavaScript and how to handle client and server-side code. It is still in development, with many features planned. Currently, there are three levels available with increasing difficulty.
--- js/transition-page.js
+++ js/transition-page.js
@@ -2,11 +2,12 @@ $(function(){
   if(window.history && window.history.pushState){
     var $mainContent = $("#content"),
         $pageWrap    = $("#post");
+    
+    loadImages(function(){
+      $pageWrap.height($pageWrap.height());
+    });
 
-    $pageWrap.height($pageWrap.height());
-    console.log("content: " + $mainContent.height() + ", page: " + $pageWrap.height());
-
-    $("#links").on("click", "a", function(e) {
+    $(document).on("click", "a.page-link", function(e) {
       link = $(this).attr("href"); //gets the address of new web page (from button/link click)
       if(window.location.pathname !== link){
         history.pushState(null, null, link);
@@ -20,25 +21,27 @@ $(function(){
     $mainContent //get div that needs to be changed
       .fadeOut(200, function() { //fade out over 200 ms, then...
         $mainContent.load(href + " #content", function() { //get new div from new page and set div to it
-          var images = $mainContent.find("img"),
-              numImages = images.length;
-
-          images.one("load", function(){
-            if(--numImages === 0) showContent(); //all images done
-          }).each(function(){
-            if(this.complete) $(this).on("load"); //if the image was cached
+          loadImages(function(){
+            $mainContent.fadeIn(200); //fade back content
+            $pageWrap.animate({
+              height: $mainContent.height() + "px" //re-adjust height of div
+            });
           });
-
-          if(images.length === 0) showContent(); //no images to load
         });
       });
   }
 
-  function showContent(){
-    $mainContent.fadeIn(200); //fade back content
-    $pageWrap.animate({
-      height: $mainContent.height() + "px" //re-adjust height of div
+  function loadImages(callback){
+    var images = $mainContent.find("img"),
+        numImages = images.length;
+
+    images.one("load", function(){
+      if(--numImages === 0) callback(); //all images done
+    }).each(function(){
+      if(this.complete) $(this).on("load"); //if the image was cached
     });
+
+    if(images.length === 0) callback(); //no images to load
   }
 
   $(window).bind('popstate', function(){
