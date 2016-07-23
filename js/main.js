e75f5977408d08b3680ea0a41401f8845fcd6a50
LenKagamine <micklepickle98@gmail.com>
2016-07-23 18:44:11 -0400

    Add lightbulb switch to toggle background

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $(function(){
-  var $bg = $("#toggle-bg");
+  var $bg = $(".toggle-bg");
   var $fp = $("#full-page");
+  var $wr = $(".wrapper");
+  var $ct = $(".container");
+
+  var state = 1;
 
   $bg.click(function(){
-    if($bg.html() === "DARK"){
+    state = !state|0;
+    $fp.css("opacity", state);
+    if(state === 0){
+      $wr.css("background-color", "#e0e0e0");
+      $ct.css("background-color", "#eaeaea");
+    }
+    else{
+      $wr.css("background-color", "transparent");
+      $ct.css("background-color", "#fff");
+    }
+    /*if($bg.html() === "DARK"){
       $fp.css("opacity", 0);
       $bg.html("LIGHT");
     }
     else{
       $fp.css("opacity", 1);
       $bg.html("DARK");
-    }
+    }*/
   });
 });
