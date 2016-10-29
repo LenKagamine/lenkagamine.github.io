9a67ce601271add1cf2e0a931b01c641be56f013
Michael Kim <micklepickle98@gmail.com>
2016-10-29 16:40:56 -0400

    Change lightbulb position and fade colors

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
+---
+---
 $(function(){
   var $togglebg = $(".toggle-bg");
   var $background = $("#full-page");
   var $wrapper = $(".wrapper");
   var $container = $(".container");
 
+  var $sitetitle = $("#site-title");
+
+  var menulinks = $("#menu");
+
   var state = 1;
 
   $togglebg.click(function(){
     state = !state|0;
     $background.css("opacity", state);
-    if(state === 0){
-      $wrapper.css("background-color", "#e0e0e0");
+    if(state === 0){ //dark
+      //$wrapper.css("background-color", "#e0e0e0");
+      $sitetitle.css("color", "#fff");
       $container.css("background-color", "#eaeaea");
+      menulinks.children().css("color", "#fff");
     }
-    else{
-      $wrapper.css("background-color", "transparent");
+    else{ //light
+      $sitetitle.css("color", "#424242"); //color is "$grey-color-dark" in _cariable.scss
+      //$wrapper.css("background-color", "transparent");
       $container.css("background-color", "#fff");
+      menulinks.children().css("color", "#111");
     }
   });
 });
---
+++ resume.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: Resume
+permalink: /resume/
+---
+
+Here's my resume
